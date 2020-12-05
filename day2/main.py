2aaaa0c2c2c09545a14d49b71d86d558adb1f998
delivey <deliveydev@gmail.com>
2020-12-05 19:55:17 +0200

    added 2nd half of day2

--- day2/main.py
+++ day2/main.py
@@ -5,6 +5,7 @@ with open('input.txt', 'r') as f: # Creates the entries list
         line = line.strip()
         passwords.append(line)
 
+# 1
 valids = 0
 
 for i in passwords:
@@ -27,3 +28,28 @@ for i in passwords:
         valids += 1
 
 print(valids)
+
+# 2
+new_valids = 0
+for i in passwords:
+    data = i.split(': ')
+    requirements = data[0]
+    password = data[1]
+
+    req_data = requirements.split(" ")
+
+    limits = req_data[0]
+    letter = req_data[1]
+
+    limits_data = limits.split("-")
+    position_1 = int(limits_data[0]) - 1
+    position_2 = int(limits_data[1]) - 1
+
+    positions = [pos for pos, char in enumerate(password) if char == letter]
+
+    # Shouldn't do this, but it works
+    if position_1 in positions and position_2 not in positions or position_2 in positions and position_1 not in positions:
+        print(positions, "VALID")
+        new_valids += 1
+
+print(new_valids)
