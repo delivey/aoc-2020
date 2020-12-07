8727b8048b8c47f513b1c15c9c368215b9bb665e
delivey <deliveydev@gmail.com>
2020-12-07 17:09:07 +0200

    added half 2 of day 6

--- day6/main.py
+++ day6/main.py
@@ -11,3 +11,21 @@ for group in groups:
     total += len(string)
 
 print(total)
+
+# 2
+
+new_total = 0
+for group in groups:
+
+    people_in_group = len(group.split("\n"))
+    group = group.replace("\n", " ")
+
+    answers = list(group)
+    unique_answers = list(set(answers))
+    for i in unique_answers:
+        people_answered = answers.count(i)
+        if people_in_group == people_answered:
+            new_total += 1
+
+print(new_total)
+    
